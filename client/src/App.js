bcd587e3ebb9571be0165804dc5fd05944169150
Lam Truong <nl2791999@gmail.com>
2022-05-05 17:03:54 -0500

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:3000");
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
